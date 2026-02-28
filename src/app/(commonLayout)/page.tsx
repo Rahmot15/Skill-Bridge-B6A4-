31a86874577e2761300ea1c48fabfa7b451a105c
Rahmot15 <mdrahmatulla666@gmail.com>
2026-02-28 17:46:22 +0600

    update Home page

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,4 +1,3 @@
-import Courses from "@/components/modules/HomePages/Courses";
 import CTA from "@/components/modules/HomePages/CTA";
 import FeaturedTutors from "@/components/modules/HomePages/featured-tutors";
 import Features from "@/components/modules/HomePages/Features";
@@ -9,16 +8,11 @@ import Testimonials from "@/components/modules/HomePages/Testimonials";
   export default async function Home() {
   return (
     <div className="">
-      {/* <HeroBanner/> */}
       <Hero/>
       <FeaturedTutors />
-      {/* <TopTutorsSection/> */}
       <Features/>
-      {/* <HowItWorksSection/> */}
-      <Courses/>
-      {/* <CategoriesSection/> */}
+      {/* <Courses/> */}
       <Pricing/>
-      {/* <TestimonialsSection/> */}
       <Testimonials/>
       <CTA/>
     </div>
